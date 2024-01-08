6e690fe7e482054f78b24e1c8b7d427aaf41fcf3
HoussineTabi <0620036304ht@gmail.com>
2024-01-08 10:56:33 +0000

    tasks

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        return the object based on the class and the
+        ID or None if not exest
+        """
+        if cls is None:
+            return None
+        objs = self.all(cls)
+        for obj in objs.values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """ count the object in a specific class """
+        return len(self.all(cls))
